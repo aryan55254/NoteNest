78a578241632d99d3da0a655cfc17090b74ec342
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-07-18 11:51:16 +0530

    initial backend setup with express and mongoose configures basic server and establishes a database connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log(" MONGO DB connection established succesfully ");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from the notenest backend");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`);
+});
