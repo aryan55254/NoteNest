a0fd43acfc6ed47599015df77d9322060b6fe37e
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-07-20 15:47:09 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 connectDB();
 
 const notesrouter = require("./routes/notes");
