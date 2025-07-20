744b9ce70f135a9a6b84c070099163e104eed98e
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-07-20 19:43:53 +0530

    workkkk

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://note-nest-fe.vercel.app',
+  origin: 'https://note-nest-fe.vercel.app/',
    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
