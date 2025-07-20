f0b3dac1ee47b049848129966d4de18b9be2ac00
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-07-20 18:56:19 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connectDB = require("./db/connectdb");
 const cors = require("cors");
-const path = require('path');
 require("dotenv").config();
 
 const app = express();
@@ -10,21 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// --- DEPLOYMENT CODE ---
-// This code serves the static files from the React frontend
-if (process.env.NODE_ENV === "production") {
-  // We are creating an absolute path to the frontend's dist folder
-  const frontendDistPath = path.resolve(__dirname, "..", "frontend", "dist");
-
-  // Serve the static files from that absolute path
-  app.use(express.static(frontendDistPath));
-
-  // For any other request, send back the index.html file from that absolute path
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(frontendDistPath, "index.html"));
-  });
-}
-// --- END DEPLOYMENT CODE ---
 connectDB();
 
 const notesrouter = require("./routes/notes");
